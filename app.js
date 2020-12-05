2db0b1d971cbc63cf4f68ba51b48877167fd364d
chandan-kj <61055870+chandan-kj@users.noreply.github.com>
2020-12-05 15:14:29 +0530

    feature: Added Url

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#translate-btn");
 var txtOutput = document.querySelector("#output-box");
 
+var serverUrl = "https://api.funtranslations.com/translate/dothraki.json"
+
+function urlConstruct(text) {
+    return serverUrl + "?" + "text=" + text
+}
+
+function clickHandler() {
+    var textInput = txtInput.value;
+
+    fetch(urlConstruct(textInput))
+        .then(response => response.json())
+        .then(json => {
+            var textTranslated = json.contents.translated;
+            txtOutput.innerHTML = textTranslated
+        })
+
+}
+
+btnTranslate.addEventListener("click", clickHandler);
